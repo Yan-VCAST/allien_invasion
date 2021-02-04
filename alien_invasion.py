d119749660027c4a4d078f3ce13f342b26b0755a
Yan-VCAST <yanlu_sh@163.com>
2021-02-04 13:33:18 +0800

    Add codes to control alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,20 +11,21 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('陆小宝打怪兽！')
 
-    # 创建一艘飞船
+    # 创建一艘飞船,一个子弹编组，和一个外星人编组
     ship = Ship(ai_settings, screen)
-    #创建一个用于存储子弹的编组
     bullets = Group()
+    aliens = Group()
 
-    #设置背景色
-    bg_color = (ai_settings.bg_color)
+    #创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     #开始游戏的主循环
     while True:
         #监视键盘和鼠标事件
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_bullets(aliens, bullets)
+        gf.update_alien(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
                 
 run_game()
--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ class Settings():
         # 屏幕设置
         self.screen_width = 1000
         self.screen_height = 600
-        self.bg_color = (230,240,220)
+        self.bg_color = (130,240,220)
 
         # 飞船的设置
         self.ship_speed_factor = 0.5
@@ -18,4 +18,10 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = 60,60,60
         self.bullets_allowed = 5
+
+        #外星人设置
+        self.alien_speed_factor = 0.1
+        self.fleet_drop_speed = 10
+        #fleet_direction为1表示向右移，-1表示向左移
+        self.fleet_direction = 1
 
